49d53e10f55a624dea3dd08c5ccba792f33187d6
MatijaPuca <pucarevic.m@gmail.com>
2022-06-18 00:09:32 -0400

    fixed the issue

--- login_page.php
+++ login_page.php
@@ -3,7 +3,7 @@ if(isset($_POST['submit']))
 {
     if($_POST["uname"]=="matija.pucarevic" && $_POST["psw"]=="Matija123")
     {
-        header("Location: https://master--voluble-griffin-819d77.netlify.app/mapOpenLayer.html");
+        echo file_get_contents("mapOpenLayer.html");
     }
 }
 ?>
